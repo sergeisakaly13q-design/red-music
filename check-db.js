35c64147c2d3fe95565df0e0a1db69a174bf6bd0
sergeisakaly13q-design <sergeisakaly13q@gmail.com>
2026-08-13 23:04:13 +0300

    Delete check-db.js

--- check-db.js
+++
@@ -1,9 +0,0 @@
-const { db, ensureSchema } = require("./db/database");
-try {
-  ensureSchema();
-  const users = db.prepare("SELECT id, username, display_name, created_at FROM users ORDER BY id DESC").all();
-  console.table(users);
-  console.log(`Всего пользователей: ${users.length}`);
-} finally {
-  db.close();
-}
